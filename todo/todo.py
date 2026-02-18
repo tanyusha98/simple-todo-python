7a82163667fe20ea606a7d701996f366fb89ea71
tanya98 <128077502+tanyusha98@users.noreply.github.com>
2026-02-18 17:41:39 +0300

    Add main execution block

--- todo/todo.py
+++ todo/todo.py
@@ -8,3 +8,9 @@ def list_tasks():
 def complete_task(index):
     if 0 <= index < len(tasks):
         tasks[index]["done"] = True
+
+if __name__ == "__main__":
+    add_task("Learn GitHub")
+    add_task("Write code")
+    complete_task(0)
+    print(list_tasks())
